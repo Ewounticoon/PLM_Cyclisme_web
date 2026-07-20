8c182beeef9b8e0ecace8e56088edfa9be961613
Ewounticoon <Ewounticoon@users.noreply.github.com>
2026-07-20 21:56:33 +0000

    deploy: Ewounticoon/Appli_PLM_Cyclisme@a16460eaa3cd266c78461920d6da342921950a5f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2050202110" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2666482202" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
